01480a960a376f0ae90fc956b4ae64bc2291c428
EC2 Default User <ec2-user@ip-172-31-39-180.ec2.internal>
2021-07-17 21:24:21 +0000

    nuevos cambios amigos nombres

--- lambda_function.py
+++ lambda_function.py
@@ -4,19 +4,19 @@ from boto3.dynamodb.conditions import Key
 
 DYNAMO_BD = os.environ['DYNAMO_BD']
 
-# Lore, aleja, walter, andres, geny, juand, juan, hernan, lucy, julian y toooooodos
+# Hola amigos como estan?
 
 class DynamoAccessor:
     def __init__(self, dynamo_table):
         dynamo_db = boto3.resource('dynamodb')
         self.table = dynamo_db.Table(dynamo_table)
 
-    def get_data_from_dynamo(self, cc):
-        response = self.table.query(KeyConditionExpression=Key('cc').eq(cc))
+    def get_data_from_dynamo(self, curp):
+        response = self.table.query(KeyConditionExpression=Key('curp').eq(curp))
         return response["Items"][0] if any(response["Items"]) else None
 
 def lambda_handler(event, context):
     dynamo_backend = DynamoAccessor(DYNAMO_BD)
-    db_element = dynamo_backend.get_data_from_dynamo(event['cc'])
+    db_element = dynamo_backend.get_data_from_dynamo(event['curp'])
     return db_element
 
